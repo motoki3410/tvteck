2fe603a8b305bae0c918018535a919e37083ec3a
Motoki <windhole922@gmail.com>
2025-08-09 02:58:59 +0900

    add entry point and argument parsing to tvteck client script

--- src/tvteck.py
+++ src/tvteck.py
@@ -1,2 +1,9 @@
+import argparse
+
+
 def main():
+    parser = argparse.ArgumentParser(description="TVTeck Client Script")
+    parser.add_argument("--config", type=str, help="Path to the configuration file")
+    args = parser.parse_args()
+
     print("This is the main entry point for the tvteck client script.")
